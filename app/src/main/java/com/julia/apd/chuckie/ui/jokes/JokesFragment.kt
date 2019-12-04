0b5ecb5b89e47b7404082c40066ed4e8abc286aa
Julia Segal <julia@flydiem.com>
2019-12-04 10:51:01 +0000

    App skeleton, ui.

--- app/src/main/java/com/julia/apd/chuckie/ui/jokes/JokesFragment.kt
+++ app/src/main/java/com/julia/apd/chuckie/ui/jokes/JokesFragment.kt
@@ -1,4 +1,4 @@
-package com.julia.apd.chuckie.ui.home
+package com.julia.apd.chuckie.ui.jokes
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.julia.apd.chuckie.R
 
-class HomeFragment : Fragment() {
+class JokesFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var viewModel: JokesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
+        viewModel =
+            ViewModelProviders.of(this).get(JokesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_jokes_list, container, false)
+        val textView: TextView = root.findViewById(R.id.text_joke_list_text)
+        viewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/julia/apd/chuckie/ui/jokes/JokesViewModel.kt
+++ app/src/main/java/com/julia/apd/chuckie/ui/jokes/JokesViewModel.kt
@@ -1,13 +1,13 @@
-package com.julia.apd.chuckie.ui.home
+package com.julia.apd.chuckie.ui.jokes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class JokesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
